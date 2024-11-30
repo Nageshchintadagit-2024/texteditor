3367aa5ca425895fbe7a4a120c7adff4c414e8a1
nagesh <chintadanagesh9@gmail.com>
2024-11-30 11:21:53 +0000

    createclasscomponent

---
+++ src/components/TextEditor/index.js
@@ -0,0 +1,17 @@
+import {Component} from "react"
+
+
+import {
+MainAppContainer,
+AppContainer,
+ImageContainer,
+TextEditorContainer,
+TextEditorHeading,
+WebsiteImage,
+TextsListContainer,
+TextAreaElement
+} from "./styledComponents"
+
+
+class Texteditor
+
---
+++ src/components/TextEditor/styledComponents.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const MainAppContainer = styled.div`
+
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+justify-content:center;
+min-height:100vh;
+background-color:#25262c;
+max-width:1200px;
+`
+export const AppContainer = styled.div`
+
+display:flex;
+flex-direction:row;
+align-items:center;
+min-height:80vh;
+background-color:#1b1c22;
+max-width:90%;
+padding:24px;
+`
+
+export const ImageContainer = styled.div`
+
+display:flex;
+flex-direction:column;
+align-items:center;
+background-color:transparent;
+
+`
+export const TextEditorContainer = styled.div`
+display:flex;
+flex-direction:column;
+background-color:#25262c;
+border:1px solid #cbd5e1;
+border-radius:16px;
+`
+
+export const TextEditorHeading = styled.h1`
+
+color:white;
+font-size:36px;
+font-weight:bold;
+font-family:"Roboto";
+
+`
+
+export const WebsiteImage = styled.img`
+
+width:90%;
+height:60%;
+
+`
+
+export const TextsListContainer = styled.div`
+
+padding-left:0px;
+display:flex;
+flex-direction:row;
+align-items:center;
+padding-left:24px;
+padding-right:24px;
+padding-top:10px;
+padding-botton:10px;
+
+`
+
+export const TextAreaElement = styled.textarea`
+
+background-color:transparent;
+
+`
